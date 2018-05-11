abad937fd723649dfd23bade6afac7c14074d514
nomanabdulla <noman.fcibappy@gmail.com>
2018-05-12 03:50:12 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+const mainArea = document.createElement('div'),
+container = document.createElement('div'),
+postCreateArea = document.createElement('div'),
+postCommentArea = document.createElement('div');
+
+container.className = 'container';
+mainArea.className = 'main-block';
+postCreateArea.className = 'post-create-area';
+postCommentArea.className = 'post-comment-area';
+
