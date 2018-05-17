84eb201685e097eb8cd8e05b5fbb026cc6a72459
menomanabdulla <noman.fcibappy@gmail.com>
2018-05-17 19:12:03 +0600

    Post add module added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ mainArea = document.createElement('div'),
 postCreateArea = document.createElement('div'),
 postArea = document.createElement('div'),
 postCommentArea = document.createElement('div');
+distinctPost = document.createElement('div');
 
 container.className = 'container';
 mainArea.className = 'main-block';
 postCreateArea.className = 'post-create-area text-center p-5';
 postCommentArea.className = 'post-comment-area';
+postArea.className = "post-block";
+distinctPost.className = "single-post-block";
 h2 = document.createElement('h2');
 h2.innerHTML = 'Single Page Application';
 document.querySelector('#root').appendChild(container);
@@ -17,12 +20,58 @@ mainArea.appendChild(postCreateArea);
 mainArea.appendChild(postArea);
 mainArea.appendChild(postCommentArea);
 postCreateArea.appendChild(h2);
+
+//profile settings
+let profileObj = {
+    profilePhoto: '3.jpg',
+    profileName:  "Noman Abdulla"
+}
 postCreateArea.innerHTML = `
+    <div class="top-bar-block">
+        <div class="top-left-block top-content-block text-left">
+            <form action="#" method="get" class="search-form">
+                <div class="input-box">
+                    <input type="text" value="" required="" name="s" placeholder="Search...">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+        <div class="top-right-block top-content-block text-right">
+            <div class="profile-block">
+                <div class="profile-photo">
+                    <a href="#">
+                        <img src="assets/images/3.jpg" alt="img" class="img-responsive profile-img">
+                    </a>
+                </div>
+                <h4><a href="#">${profileObj.profileName}</a></h4>
+                <div class="settings-block">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-cog"></i>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">
+                                <div class="custom-file" id="customFile" lang="es">
+                                    <input type="file" class="custom-file-input" id="imageInput" aria-describedby="fileHelp">
+                                    <label class="custom-file-label" for="exampleInputFile">
+                                    
+                                    </label>
+                                </div>
+                            </a>
+                            <a class="dropdown-item" href="#">
+                               <span class="name-change-feild"> Change Name </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="post-create-block">
         <div class="p-main">
             <div class="profile-block">
                 <div class="profile-photo">
-                    <img src="assets/images/profile.jpg" alt="img" class="img-responsive">
+                    <img src="assets/images/3.jpg" alt="img" class="img-responsive profile-img">
                 </div>
             </div>
             <textarea class="form-control" id="postInput" rows="7" placeholder="Whats on your mind..?"></textarea>
@@ -81,6 +130,8 @@ postCreateArea.innerHTML = `
 
 // post creation module
 
+//background color settings
+let bgColor = "#fff";
 const ColorOption1 = document.querySelector('.color');
 ColorOption1.onclick = function(){
     var elems = document.querySelectorAll(".active-bg-color");
@@ -88,7 +139,7 @@ ColorOption1.onclick = function(){
         el.classList.remove("active-bg-color");
     });
     ColorOption1.classList.toggle('active-bg-color');
-
+    bgColor = ColorOption1.getAttribute("data-color");
 }
 const ColorOption2 = document.querySelector('.color-two');
 ColorOption2.onclick = function(){
@@ -97,6 +148,7 @@ ColorOption2.onclick = function(){
         el.classList.remove("active-bg-color");
     });
     ColorOption2.classList.toggle('active-bg-color');
+    bgColor = ColorOption2.getAttribute("data-color");
 }
 const ColorOption3 = document.querySelector('.color-three');
 ColorOption3.onclick = function(){
@@ -105,6 +157,7 @@ ColorOption3.onclick = function(){
         el.classList.remove("active-bg-color");
     });
     ColorOption3.classList.toggle('active-bg-color');
+    bgColor = ColorOption3.getAttribute("data-color");
 }
 const ColorOption4 = document.querySelector('.color-four');
 ColorOption4.onclick = function(){
@@ -113,6 +166,7 @@ ColorOption4.onclick = function(){
         el.classList.remove("active-bg-color");
     });
     ColorOption4.classList.toggle('active-bg-color');
+    bgColor = ColorOption4.getAttribute("data-color");
 }
 const ColorOption5 = document.querySelector('.color-five');
 ColorOption5.onclick = function(){
@@ -121,25 +175,79 @@ ColorOption5.onclick = function(){
         el.classList.remove("active-bg-color");
     });
     ColorOption5.classList.toggle('active-bg-color');
+    bgColor = ColorOption5.getAttribute("data-color");
 }
 
 //font-family block
 let fontFamily = 'serif';
 document.getElementById("fontFamily").onchange = function () {
     fontFamily= this.options[this.selectedIndex].getAttribute("data-font-family");
-    console.log(fontFamily);
 };
 
 //font-size block
 let fontSize = '14';
 document.getElementById("fontSize").onchange = function () {
     fontSize= this.options[this.selectedIndex].getAttribute("data-font-size");
-    console.log(fontSize);
+    fontSize = fontSize+'px';
 };
 
 //font-size block
 let fontColor = '#111';
 document.getElementById("fontColor").onchange = function () {
-    fontSize= this.options[this.selectedIndex].getAttribute("data-font-color");
-    console.log(fontSize);
+    fontColor= this.options[this.selectedIndex].getAttribute("data-font-color");
 };
+
+//profile settings block
+document.getElementById("imageInput").onchange = function () {
+    var elems = document.querySelectorAll(".profile-img");
+    var resultImg = (this.files[0]);
+    var reader  = new FileReader();
+    reader.onloadend = function () {
+        [].forEach.call(elems, function(el) {
+            el.src = reader.result;
+        });
+    }
+    if (resultImg) {
+        reader.readAsDataURL(resultImg);
+    } else {
+        [].forEach.call(elems, function(el) {
+            el.src = "";
+        });
+    }
+};
+
+let postButton = document.getElementById("postButton");
+postButton.onclick = function(){
+    let postContent = document.getElementById("postInput").value;
+    distinctPost.innerHTML = `
+        <div class="post-inner-block">
+            <div class="profile-block">
+                <div class="profile-photo">
+                    <img src="assets/images/3.jpg" alt="img" class="img-responsive profile-img">
+                </div>
+                <h4><a href="#">${profileObj.profileName}</a></h4>
+            </div>
+            <div class="post-body" style="background:${bgColor}">
+                <p style="color:${fontColor};font-family:${fontFamily};font-size:${fontSize};">
+                    ${postContent}
+                </p>
+            </div>
+            <div class="post-bottom-block">
+                <div class="like-block">
+                    <a href="#">
+                        <i class="far fa-thumbs-up"></i>
+                        <span>4 likes</span>
+                    </a>
+                </div>
+                <div class="comment-block">
+                    <a href="#">
+                        <i class="far fa-comment"></i>
+                        <span>2 Comments</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    `;
+    postArea.appendChild(distinctPost);
+}
+
